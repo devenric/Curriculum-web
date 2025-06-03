a52b3c0da8a4c38528db56cf3527e8b64d5d7e71
= <=>
2025-06-03 11:56:03 +0200

    2/6/25

---
+++ script.js
@@ -0,0 +1,7 @@
+const aboutme ={
+    name: "John Doe",
+    age: 30,
+    profession: "Software Developer",
+    hobbies: ["coding", "reading", "gaming"],
+    skills: ["JavaScript", "Python", "React"]
+}
